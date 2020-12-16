c125a2f67800fbe90902c23199fcc9471fb4acb4
ahorlach <73469756+ahorlach@users.noreply.github.com>
2020-12-16 17:27:13 +0200

    test

--- cypress/integration/connex/pageElements.js
+++ cypress/integration/connex/pageElements.js
@@ -1,11 +1,4 @@
-const
-    user = Cypress.env().buyer.admin.username,
-    pw = Cypress.env().buyer.admin.password,
-    entityCrn = Cypress.env().buyer.crn,
-    companyName = Cypress.env().buyer.companyName,
-    reportNameField = '#reportName',
-    sandwichMenu = 'i[class="gc-icon gc-btn__icon gc-icon--large"]',
-    parametersButton = 'button[title="Parameters"]';
+
 
 
 export let getReportByNameFromTable = (reportName) => {
